8e4be0b6a37bb264f87ed0bc60151ccbd2c3087b
Hector Morales <tuntu99@gmail.com>
2020-05-21 21:45:20 -0600

    Aqui estan las configuraciones de container

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.lightBlue,
+        body: SafeArea(
+          child: Container(
+            child: Text('Hello'),
+            color: Colors.white,
+            margin: EdgeInsets.only(left: 30.0),
+            padding: EdgeInsets.fromLTRB(30.0, 20.0, 30.0, 20.0),
+            height: 100.0,
+            width: 100.0,
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
