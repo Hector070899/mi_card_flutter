60cd080c1adec3b153ac8c7e2a5806d122b86b1a
Hector Morales <tuntu99@gmail.com>
2020-05-22 01:40:37 -0600

    My card has been created

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,44 +12,76 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal, //listo
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Container(
-                color: Colors.red,
-                padding: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
-                width: 100.0,
-                //width: double.infinity,
-                //child: Text('Container1'),
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('images/hector.jpg'),
+            ),
+            Text(
+              'Hector Morales',
+              style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico'),
+            ),
+            Text(
+              'FLUTTER DEVELOPER',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.5,
+                  fontFamily: 'Source Sans Pro'),
+            ),
+            SizedBox(
+              height: 10.0,
+              width: 180,
+              child: Divider(
+                color: Colors.white,
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    //width: double.infinity,
-                    width: 100.0,
-                    height: 100.0,
-                    padding: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
-                    color: Colors.yellow,
-                    //child: Text('Container2'),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: Padding(
+                  padding: const EdgeInsets.all(2.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal.shade900,
+                    ),
+                    title: Text(
+                      '+505 83731668',
+                      style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontSize: 20.0,
+                          fontFamily: 'Source Sans Pro'),
+                    ),
+                  )),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: Padding(
+                padding: const EdgeInsets.all(2.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.alternate_email,
+                    color: Colors.teal.shade900,
                   ),
-                  Container(
-                    color: Colors.green,
-                    height: 100.0,
-                    width: 100.0,
-                  )
-                ],
+                  title: Text(
+                    'hector.moralesv@gmail.com',
+                    style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontSize: 20.0,
+                        fontFamily: 'Source Sans Pro'),
+                  ),
+                ),
               ),
-              Container(
-                //width: double.infinity,
-                width: 100.0,
-                padding: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
-                color: Colors.blue,
-                // child: Text('Container3'),
-              ),
-            ],
-          ),
-        ),
+            )
+          ],
+        )),
       ),
     );
   }
