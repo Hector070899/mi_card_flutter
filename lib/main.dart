94cef53a564de2f8c9ceb00925d132bbc17cbeb2
Hector Morales <tuntu99@gmail.com>
2020-05-21 23:35:29 -0600

    Challenge solved

--- lib/main.dart
+++ lib/main.dart
@@ -9,37 +9,46 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.lightBlue,
+        backgroundColor: Colors.teal, //listo
         body: SafeArea(
-            child: Row(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-              color: Colors.white,
-              padding: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
-              width: 100.0,
-              //width: double.infinity,
-              child: Text('Container1'),
-            ),
-            SizedBox(
-              width: 20.0,
-            ),
-            Container(
-              //width: double.infinity,
-              width: 100.0,
-              padding: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
-              color: Colors.red,
-              child: Text('Container2'),
-            ),
-            Container(
-              //width: double.infinity,
-              width: 100.0,
-              padding: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
-              color: Colors.yellow,
-              child: Text('Container3'),
-            ),
-          ],
-        )),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                color: Colors.red,
+                padding: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
+                width: 100.0,
+                //width: double.infinity,
+                //child: Text('Container1'),
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    //width: double.infinity,
+                    width: 100.0,
+                    height: 100.0,
+                    padding: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
+                    color: Colors.yellow,
+                    //child: Text('Container2'),
+                  ),
+                  Container(
+                    color: Colors.green,
+                    height: 100.0,
+                    width: 100.0,
+                  )
+                ],
+              ),
+              Container(
+                //width: double.infinity,
+                width: 100.0,
+                padding: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
+                color: Colors.blue,
+                // child: Text('Container3'),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
